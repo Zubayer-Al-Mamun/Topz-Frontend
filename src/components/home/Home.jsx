15155aa31e04f9f4cf4bbd4b3949396ecb0e0771
Zubayer-Al-Mamun <amzubayer83@gmail.com>
2026-09-02 08:21:26 +0600

    add product shorting by categorys

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,40 +1,52 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, NavLink, useLoaderData } from "react-router-dom";
 import Product from "../Product";
 import FullWidthCarousel from "./FullWidthCarousel";
 import "./home.css";
 import BottomNavbar from "./bottom_nav/BottomNavbar";
+import { useState } from "react";
 
 export default function Home() {
     const products = useLoaderData();
+    const [cat, setCat] = useState("all");
+    console.log(cat)
 
     console.log(products)
 
     // const products = [...products_data].sort(() => Math.random() - 0.5);
 
-    const categories = ["Shirt", "T-Shirt", "Hoodie", "Drop Shoulder"];
+    const categories = ["All","Sharee","Shirt", "T-Shirt", "Hoodie"];
     // console.log(products);
 
     const slides = [
         {
             id: 1,
-            title: "View More..",
+            title: "",
             subtitle: "Subtitle",
-            image: "https://res.cloudinary.com/drtnxa49y/image/upload/v1765689968/frnyy6mvoa6pulbwwtvp.jpg",
+            image: "https://res.cloudinary.com/drtnxa49y/image/upload/v1788321643/21e70e03047119b47129730297a92b33.jpg",
             link: "",
         },
         {
             id: 2,
-            title: "Slide 2",
+            title: "",
             subtitle: "Subtitle",
-            image: "https://res.cloudinary.com/drtnxa49y/image/upload/v1765689968/frnyy6mvoa6pulbwwtvp.jpg",
+            image: "https://res.cloudinary.com/drtnxa49y/image/upload/v1788321644/871714161665ca651fd24f9a6bdd988f.jpg",
             link: "",
         },
         {
             id: 3,
-            title: "Slide 3",
+            title: "",
             subtitle: "Subtitle",
-            image: "https://res.cloudinary.com/drtnxa49y/image/upload/v1765689968/frnyy6mvoa6pulbwwtvp.jpg",
+            image: "https://res.cloudinary.com/drtnxa49y/image/upload/v1788321644/799113f5aa96a8e0df9cd52cc13fbd28.jpg",
             link: "",
+        },
+        {
+            id: 3,
+            title: "",
+            subtitle: "Subtitle",
+            image: "https://res.cloudinary.com/drtnxa49y/image/upload/v1788321645/db0f5688e8c6a68145e7d9bf0096a1c8.jpg",
+            link: "",
+
+            
         },
     ];
 
@@ -53,12 +65,13 @@ export default function Home() {
             />
 
             <div className="flex m-[10px] px-2 gap-[10px] overflow-y-hidden categorie">
-                {categories.map((cat) => (
+                {categories.map((cate) => (
                     <button
-                        key={cat}
-                        className="flex-shrink-0 bg-white px-4 py-2 rounded-xl shadow text-sm md:text-base font-medium hover:bg-blue-100"
+                        onClick={() => setCat(cate.toLowerCase())}
+                        key={cate}
+                        className={(cate.toLowerCase()  === cat) ? "flex-shrink-0 px-4 py-2 rounded-xl shadow text-sm md:text-base font-medium hover:bg-blue-100 bg-blue-400" : "flex-shrink-0 bg-white px-4 py-2 rounded-xl shadow text-sm md:text-base font-medium hover:bg-blue-100" }
                     >
-                        {cat}
+                        {cate}
                     </button>
                 ))}
             </div>
@@ -67,8 +80,12 @@ export default function Home() {
                 {/* <div>
                     <p className="bangla-text" > আসসালামু আলাইকুম </p>
                 </div> */}
-                {products.map((product, idx) => (
+                {cat === "all" && products.map((product, idx) => (
                     <Product key={idx} product={product}  idx={idx} />
+                ))}
+
+                {cat != "all" && products.filter((item)=> item.category === cat.toLowerCase()).map((product, idx) => (
+                     <Product key={idx} product={product}  idx={idx} />
                 ))}
             </div>
             <BottomNavbar/>
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import Product from "../Product";
 
 export default function Shop() {
     const products = useLoaderData();
     console.log(products);
-    return (
-        <div className=" w-full flex flex-wrap justify-center max-sm:gap-1 sm:gap-2 lg:mt-2">
-            {products.map((product, idx) => (
-                <Product key={idx} product={product}/>
-            ))}
+    const [cat, setCat] = useState("all");
+    const categories = ["All", "Sharee", "Shirt", "T-Shirt", "Hoodie"];
 
-        </div>
+    return (
+        <>
+            <div className="flex m-[10px] px-2 gap-[10px] overflow-y-hidden categorie">
+                {categories.map((cate) => (
+                    <button
+                        onClick={() => setCat(cate.toLowerCase())}
+                        key={cate}
+                        className={
+                            cate.toLowerCase() === cat
+                                ? "flex-shrink-0 px-4 py-2 rounded-xl shadow text-sm md:text-base font-medium hover:bg-blue-100 bg-blue-400"
+                                : "flex-shrink-0 bg-white px-4 py-2 rounded-xl shadow text-sm md:text-base font-medium hover:bg-blue-100"
+                        }
+                    >
+                        {cate}
+                    </button>
+                ))}
+            </div>
+
+            <div className="product-cont w-[100%] pb-[60px] flex flex-wrap justify-center max-sm:gap-1 sm:gap-2 mt-2">
+                {/* <div>
+                                <p className="bangla-text" > আসসালামু আলাইকুম </p>
+                            </div> */}
+                {cat === "all" &&
+                    products.map((product, idx) => (
+                        <Product key={idx} product={product} idx={idx} />
+                    ))}
+
+                {cat != "all" &&
+                    products
+                        .filter((item) => item.category === cat.toLowerCase())
+                        .map((product, idx) => (
+                            <Product key={idx} product={product} idx={idx} />
+                        ))}
+            </div>
+        </>
     );
 }
